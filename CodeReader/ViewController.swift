f05afcf4a696b76d67e3b34a391afceb0fb93f1e
Hiroyasu Kondo <h.kondo@stonesystem.co.jp>
2018-05-14 15:25:58 +0900

    Initial Commit

---
+++ CodeReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeReader
+//
+//  Created by stone on 2018/05/14.
+//  Copyright © 2018年 stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
